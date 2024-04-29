0445e434295cb1bcea8efc5f7e271ed92286eae6
SiddharthSai4701 <sidvsai@gmail.com>
2024-04-29 20:30:58 +0530

    Fragments created. PagerAdapter to be added

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem home, science, health, tech, entertainment, sports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
